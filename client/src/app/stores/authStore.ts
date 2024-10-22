814edb266ff529fa5525182342319a7cc8ef6b28
AlexVSi <aleksandrsid2003@gmail.com>
2024-10-22 19:34:25 +0300

    add jwt decode

--- client/src/app/stores/authStore.ts
+++ client/src/app/stores/authStore.ts
@@ -3,6 +3,7 @@ import AuthService from "@entities/auth/api/auth.service";
 import { makeAutoObservable } from "mobx";
 import axios from "axios";
 import { AuthResponse } from "@entities/auth/models/AuthResponse";
+import { jwtDecode } from "jwt-decode";
 
 export default class AuthStore {
     user = {} as IUserLogin
@@ -28,12 +29,15 @@ export default class AuthStore {
     async login(email: string, password: string) {
         try {
             const response = await AuthService.login(email, password)
-            localStorage.setItem('token', response.data.tokens.accessToken)
+            const token = response.data.accessToken
+            const tokenDecode = jwtDecode<IUserLogin>(token)
+            localStorage.setItem('token', token)
             this.setAuth(true)
-            this.setUser(response.data.user)
+            this.setUser(tokenDecode)
             return true
         } catch (e: any) {
             console.log(e.response?.data?.message);
+            console.log(e);
             return
         }
     }
@@ -41,10 +45,11 @@ export default class AuthStore {
     async registration(userData: IUserReg) {
         try {
             const response = await AuthService.registration(userData)
-            localStorage.setItem('token', response.data.tokens.accessToken)
+            const token = response.data.accessToken
+            const tokenDecode = jwtDecode<IUserLogin>(token)
+            localStorage.setItem('token', token)
             this.setAuth(true)
-            const user = response.data.user
-            this.setUser({ id: user.id, email: user.email, roles: user.roles })
+            this.setUser(tokenDecode)
             return true
         } catch (e: any) {
             console.log(e.response?.data?.message);
@@ -67,9 +72,11 @@ export default class AuthStore {
         try {
             this.setLoading(true)
             const response = await axios.get<AuthResponse>('http://127.0.0.1:8888/api/auth/refresh', { withCredentials: true })
-            localStorage.setItem('token', response.data.tokens.accessToken)
+            const token = response.data.accessToken
+            const tokenDecode = jwtDecode<IUserLogin>(token)
+            localStorage.setItem('token', token)
             this.setAuth(true)
-            this.setUser(response.data.user)
+            this.setUser(tokenDecode)
         } catch (e) {
 
         } finally {
